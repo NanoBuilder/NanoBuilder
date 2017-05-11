d5de1c74d12e61fbed2d52f928872b0c7fc1a17f
Alex Novak <alexwnovak@gmail.com>
2017-05-10 21:55:26 -0400

    Can now create default objects

--- src/NanoBuilder/NanoBuilder.Tests/ObjectBuilderTests.cs
+++ src/NanoBuilder/NanoBuilder.Tests/ObjectBuilderTests.cs
@@ -1,6 +1,24 @@
-﻿namespace NanoBuilder.Tests
+﻿using Xunit;
+using FluentAssertions;
+
+namespace NanoBuilder.Tests
 {
    public class ObjectBuilderTests
    {
+      [Fact]
+      public void Build_BuildingAnObject_ReturnsBuilderForThatType()
+      {
+         var builder = ObjectBuilder<string>.Create();
+
+         builder.Should().BeOfType<ObjectBuilder<string>>();
+      }
+
+      [Fact]
+      public void Build_BuildingAnInt_ReturnsDefaultInt()
+      {
+         int value = ObjectBuilder<int>.Create().Build();
+
+         value.Should().Be( default( int ) );
+      }
    }
 }
--- src/NanoBuilder/NanoBuilder/ObjectBuilder.cs
+++ src/NanoBuilder/NanoBuilder/ObjectBuilder.cs
@@ -1,6 +1,13 @@
 ﻿namespace NanoBuilder
 {
-   public class ObjectBuilder
+   public class ObjectBuilder<T>
    {
+      private ObjectBuilder()
+      {
+      }
+
+      public static ObjectBuilder<T> Create() => new ObjectBuilder<T>();
+
+      public T Build() => default( T );
    }
 }
