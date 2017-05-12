80995b1bb1a1dac830bf213fcfd4800690302f42
Alex Novak <alexwnovak@gmail.com>
2017-05-12 14:16:04 -0400

    +XML comments for ITypeMapper

--- src/NanoBuilder/NanoBuilder/ITypeMapper.cs
+++ src/NanoBuilder/NanoBuilder/ITypeMapper.cs
@@ -2,8 +2,21 @@
 
 namespace NanoBuilder
 {
+   /// <summary>
+   /// Represents an object that can map one kind of Type to another.
+   /// <remarks>
+   /// This is used by the <see cref="ObjectBuilder{T}"/> class when transforming constructor
+   /// parameter types into other types. For example, mapping all interfaces into mock versions of
+   /// the interface.
+   /// </remarks>
+   /// </summary>
    public interface ITypeMapper
    {
+      /// <summary>
+      /// Creates an instance that maps to the specific <see cref="Type"/>.
+      /// </summary>
+      /// <param name="type">The <see cref="Type"/> of object that needs to be mapped.</param>
+      /// <returns>An object that represents the mapped type.</returns>
       object CreateForInterface( Type type );
    }
 }
