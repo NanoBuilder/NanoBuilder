d72f6d9f43a8ffe42abf31eb85ca16fb5181e2d9
Alex Novak <alexwnovak@gmail.com>
2017-05-15 22:40:08 -0400

    +Common method that returns elements common to both, including dupes

--- src/NanoBuilder/NanoBuilder/EnumerableExtensions.cs
+++ src/NanoBuilder/NanoBuilder/EnumerableExtensions.cs
@@ -1,6 +1,16 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace NanoBuilder
 {
    internal static class EnumerableExtensions
    {
+      public static IEnumerable<T> Common<T>( this IEnumerable<T> first, IEnumerable<T> second )
+      {
+         var lookup1 = first.ToLookup( x => x );
+         var lookup2 = second.ToLookup( x => x );
+
+         return lookup1.SelectMany( l1s => lookup2[l1s.Key].Zip( l1s, ( l2, l1 ) => l1 ) );
+      }
    }
 }
