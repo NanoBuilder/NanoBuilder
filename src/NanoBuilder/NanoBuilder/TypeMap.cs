10e2c5f88c9eefa3a4868b91a910606bd1452b79
Alex Novak <alexwnovak@gmail.com>
2017-05-15 23:01:28 -0400

    Can now Add/Get instances from the TypeMap

--- src/NanoBuilder/NanoBuilder/TypeMap.cs
+++ src/NanoBuilder/NanoBuilder/TypeMap.cs
@@ -1,6 +1,36 @@
+using System;
+using System.Collections.Generic;
+
 namespace NanoBuilder
 {
    internal class TypeMap
    {
+      private readonly Dictionary<Type, Queue<object>> _mappings = new Dictionary<Type, Queue<object>>();
+
+      public void Add<TParameterType>( TParameterType parameter )
+      {
+         var parameterType = typeof( TParameterType );
+
+         if ( !_mappings.TryGetValue( parameterType, out var typeMapEntries ) )
+         {
+            typeMapEntries = new Queue<object>();
+            _mappings[parameterType] = typeMapEntries;
+         }
+
+         typeMapEntries.Enqueue( parameter );
+      }
+
+      public (object, bool) Get( Type parameterType )
+      {
+         if ( _mappings.TryGetValue( parameterType, out var queue ) )
+         {
+            if ( queue.Count > 0 )
+            {
+               return (queue.Dequeue(), true);
+            }
+         }
+
+         return (null, false);
+      }
    }
 }
