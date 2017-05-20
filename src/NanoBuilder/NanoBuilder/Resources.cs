e382644f7365dcb9ecca4d4ce144016b24014767
Alex Novak <alexwnovak@gmail.com>
2017-05-20 13:24:37 -0400

    Simplifying the resource strings by making them public const only

--- src/NanoBuilder/NanoBuilder/Resources.cs
+++ src/NanoBuilder/NanoBuilder/Resources.cs
@@ -2,22 +2,16 @@
 {
    internal static class Resources
    {
-      private const string _ambiguousConstructorMessage = "Multiple constructors were found matching the mapped types. Be more specific by mapping more types.\r\n\r\nConstructors found:\r\n{0}";
-      public static string AmbiguousConstructorMessage => _ambiguousConstructorMessage;
+      public const string AmbiguousConstructorMessage = "Multiple constructors were found matching the mapped types. Be more specific by mapping more types.\r\n\r\nConstructors found:\r\n{0}";
 
-      private const string _mapperMessage = "The interface mapper can only be set once. Did you call MapInterfacesTo() more than once?";
-      public static string MapperMessage = _mapperMessage;
+      public const string MapperMessage = "The interface mapper can only be set once. Did you call MapInterfacesTo() more than once?";
 
-      private const string _moqMapperMessage = "Unable to locate Moq.Mock type. Are you referencing Moq?";
-      public static string MoqMapperMessage = _moqMapperMessage;
+      public const string MoqMapperMessage = "Unable to locate Moq.Mock type. Are you referencing Moq?";
 
-      private const string _nsubstituteMapperMessage = "Unable to locate NSubstitute.Substitute type. Are you referencing NSubstitute?";
-      public static string NSubstituteMapperMessage = _nsubstituteMapperMessage;
+      public const string NSubstituteMapperMessage = "Unable to locate NSubstitute.Substitute type. Are you referencing NSubstitute?";
 
-      private const string _parameterMappingMessage = "No public constructors on type {0} were found to accept a parameter of type {1}";
-      public static string ParameterMappingMessage = _parameterMappingMessage;
+      public const string ParameterMappingMessage = "No public constructors on type {0} were found to accept a parameter of type {1}";
 
-      private const string _rhinoMocksMapperMessage = "Unable to locate the Rhino.Mocks.MockRepository type. Are you referencing RhinoMocks?";
-      public static string RhinoMocksMapperMessage = _rhinoMocksMapperMessage;
+      public const string RhinoMocksMapperMessage = "Unable to locate the Rhino.Mocks.MockRepository type. Are you referencing RhinoMocks?";
    }
 }
