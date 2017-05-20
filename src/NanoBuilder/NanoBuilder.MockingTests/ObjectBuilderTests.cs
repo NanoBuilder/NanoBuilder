56af9b90ae954d25f1ca80120f23c362563bb9c7
Alex Novak <alexwnovak@gmail.com>
2017-05-20 09:19:43 -0400

    Moving over the MoqMapper test
    Also had to dupe some stubs we use. So far they're duped, but we can probably move these into a common place.

--- src/NanoBuilder/NanoBuilder.MockingTests/ObjectBuilderTests.cs
+++ src/NanoBuilder/NanoBuilder.MockingTests/ObjectBuilderTests.cs
@@ -1,6 +1,20 @@
-﻿namespace NanoBuilder.MockingTests
+﻿using Xunit;
+using FluentAssertions;
+using Moq;
+using NanoBuilder.MockingTests.Stubs;
+
+namespace NanoBuilder.MockingTests
 {
    public class ObjectBuilderTests
    {
+      [Fact]
+      public void Build_OmitsInterfaceParameter_OmittedInterfaceBecomesAMoqOfThatInterface()
+      {
+         var logger = ObjectBuilder.For<Logger>()
+            .MapInterfacesTo<MoqMapper>()
+            .Build();
+
+         Mock.Get( logger.FileSystem ).Should().BeOfType<Mock<IFileSystem>>();
+      }
    }
 }
