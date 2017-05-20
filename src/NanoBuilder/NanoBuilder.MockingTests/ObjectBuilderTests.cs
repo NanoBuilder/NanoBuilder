6e38e5a25d320a46b63d62323dc443639223e32e
Alex Novak <alexwnovak@gmail.com>
2017-05-20 10:25:09 -0400

    Now throws if trying to set more than one interface mapper

--- src/NanoBuilder/NanoBuilder.MockingTests/ObjectBuilderTests.cs
+++ src/NanoBuilder/NanoBuilder.MockingTests/ObjectBuilderTests.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using System;
+using Xunit;
 using FluentAssertions;
 using Moq;
 using Rhino.Mocks.Interfaces;
@@ -8,6 +9,16 @@ namespace NanoBuilder.MockingTests
 {
    public class ObjectBuilderTests
    {
+      [Fact]
+      public void MapInterfacesTo_MultipleMappersAreUsed_ThrowsMapperException()
+      {
+         Action mapInterfacesTo = () => ObjectBuilder.For<Logger>()
+            .MapInterfacesTo<MoqMapper>()
+            .MapInterfacesTo<RhinoMocksMapper>();
+
+         mapInterfacesTo.ShouldThrow<MapperException>();
+      }
+
       [Fact]
       public void Build_OmitsInterfaceParameter_OmittedInterfaceBecomesAMoqOfThatInterface()
       {
--- src/NanoBuilder/NanoBuilder/Resources.cs
+++ src/NanoBuilder/NanoBuilder/Resources.cs
@@ -5,6 +5,9 @@
       private const string _ambiguousConstructorMessage = "Multiple constructors were found matching the mapped types. Be more specific by mapping more types.\r\n\r\nConstructors found:\r\n{0}";
       public static string AmbiguousConstructorMessage => _ambiguousConstructorMessage;
 
+      private const string _mapperMessage = "The interface mapper can only be set once. Did you call MapInterfacesTo() more than once?";
+      public static string MapperMessage = _mapperMessage;
+
       private const string _parameterMappingMessage = "No public constructors on type {0} were found to accept a parameter of type {1}";
       public static string ParameterMappingMessage = _parameterMappingMessage;
 
