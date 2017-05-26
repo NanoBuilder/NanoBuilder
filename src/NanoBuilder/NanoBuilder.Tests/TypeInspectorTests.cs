5faea5c7703bd9c02173f3eedad36aede4e90ff9
Alex Novak <alexwnovak@gmail.com>
2017-05-26 15:44:55 -0400

    Retrofitting some tests to TypeInspector

---
+++ src/NanoBuilder/NanoBuilder.Tests/TypeInspectorTests.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+using FluentAssertions;
+
+namespace NanoBuilder.Tests
+{
+   public class TypeInspectorTests
+   {
+      [Fact]
+      public void GetType_PassesValidTypeName_GetsTheRightType()
+      {
+         var typeInspector = new TypeInspector();
+
+         var type = typeInspector.GetType( "System.Int32" );
+
+         type.Should().Be( typeof( int ) );
+      }
+
+      [Fact]
+      public void GetType_PassesInvalidTypeName_ReturnsNull()
+      {
+         var typeInspector = new TypeInspector();
+
+         var type = typeInspector.GetType( "System.NotAThing" );
+
+         type.Should().BeNull();
+      }
+   }
+}
