becfb8a882d58eb5b7890ed611cfb17f4e368445
Alex Novak <alexwnovak@gmail.com>
2017-05-27 14:25:45 -0400

    Moving all the "mocking" integration tests into acceptance tests
    This is really what they are. Also allowed me to remove the whole "mocking" test project, which was a little too specialized.

--- src/NanoBuilder/NanoBuilder.AcceptanceTests/MapInterfacesScenarios.cs
+++ src/NanoBuilder/NanoBuilder.AcceptanceTests/MapInterfacesScenarios.cs
@@ -1,7 +1,10 @@
 ﻿using Xunit;
 using FluentAssertions;
 using Moq;
+using NSubstitute.Core;
+using Rhino.Mocks.Interfaces;
 using NanoBuilder.Stubs;
+using FakeItEasy.Creation;
 
 namespace NanoBuilder.AcceptanceTests
 {
@@ -19,6 +22,46 @@ namespace NanoBuilder.AcceptanceTests
 
          Mock.Get( fileSystemCache.FileSystem ).Should().BeOfType<Mock<IFileSystem>>();
          fileSystemCache.CacheSize.Should().Be( cacheSize );
+      }
+
+      [Fact]
+      public void CanMapInterfacesToMoqMocks()
+      {
+         var logger = ObjectBuilder.For<Logger>()
+            .MapInterfacesTo<MoqMapper>()
+            .Build();
+
+         Mock.Get( logger.FileSystem ).Should().BeOfType<Mock<IFileSystem>>();
+      }
+
+      [Fact]
+      public void CanMapInterfacesToRhinoMocks()
+      {
+         var logger = ObjectBuilder.For<Logger>()
+            .MapInterfacesTo<RhinoMocksMapper>()
+            .Build();
+
+         logger.FileSystem.GetType().GetInterfaces().Should().Contain( typeof( IMockedObject ) );
+      }
+
+      [Fact]
+      public void CanMapInterfacesToNSubstituteMocks()
+      {
+         var logger = ObjectBuilder.For<Logger>()
+            .MapInterfacesTo<NSubstituteMapper>()
+            .Build();
+
+         logger.FileSystem.GetType().GetInterfaces().Should().Contain( typeof( ICallRouter ) );
+      }
+
+      [Fact]
+      public void CanMapInterfacesToFakeItEasyMocks()
+      {
+         var logger = ObjectBuilder.For<Logger>()
+            .MapInterfacesTo<FakeItEasyMapper>()
+            .Build();
+
+         logger.FileSystem.GetType().GetInterfaces().Should().Contain( typeof( ITaggable ) );
       }
    }
 }
