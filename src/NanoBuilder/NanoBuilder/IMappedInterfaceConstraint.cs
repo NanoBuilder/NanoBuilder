c319963d6ed5f222dbb26a150daa9e3a03e97d6f
Alex Novak <alexwnovak@gmail.com>
2017-05-27 09:45:33 -0400

    Extracting a more specialized interface that allows interface mapping
    The idea is that mapping an interface will return a different kind of interface afterward, so you actually CAN'T call "map" again, because the returned interface WON'T have the map method.

---
+++ src/NanoBuilder/NanoBuilder/IMappedInterfaceConstraint.cs
@@ -0,0 +1,16 @@
+﻿namespace NanoBuilder
+{
+   /// <summary>
+   /// This allows you decide how interfaces types can be initialized by default.
+   /// </summary>
+   /// <typeparam name="T">The type of object to build.</typeparam>
+   public interface IMappedInterfaceConstraint<T>
+   {
+      /// <summary>
+      /// Configures how interface types should be initialized by default. 
+      /// </summary>
+      /// <typeparam name="TMapperType">The type of mapper to transform objects.</typeparam>
+      /// <returns>The same <see cref="ParameterComposer{T}"/>.</returns>
+      IParameterComposer<T> MapInterfacesTo<TMapperType>() where TMapperType : ITypeMapper;
+   }
+}
