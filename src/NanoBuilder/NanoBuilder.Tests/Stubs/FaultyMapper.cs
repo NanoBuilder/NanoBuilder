2f14d37a981dd9e6fe0b6bc7306318c60bd6dc19
Alex Novak <alexwnovak@gmail.com>
2017-05-26 22:23:06 -0400

    MapperFactory now handles creating mappers

---
+++ src/NanoBuilder/NanoBuilder.Tests/Stubs/FaultyMapper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NanoBuilder.Tests.Stubs
+{
+   internal class FaultyMapper : ITypeMapper
+   {
+      public object CreateForInterface( Type type )
+      {
+         throw new NotImplementedException();
+      }
+   }
+}
