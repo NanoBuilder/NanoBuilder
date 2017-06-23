d23daeaba96f07555669ce63a2b0c55b34c04923
Alex Novak <alexwnovak@gmail.com>
2017-06-22 22:11:08 -0400

    +Constructor helper class to generate mocks

---
+++ src/NanoBuilder/NanoBuilder.UnitTests/Helpers/Constructor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Moq;
+
+namespace NanoBuilder.UnitTests.Helpers
+{
+   internal static class Constructor
+   {
+      public static Mock<IConstructor> With( Type type )
+      {
+         var constructorMock = new Mock<IConstructor>();
+         
+         constructorMock.SetupGet( c => c.Type ).Returns( type );
+
+         return constructorMock;
+      }
+   }
+}
