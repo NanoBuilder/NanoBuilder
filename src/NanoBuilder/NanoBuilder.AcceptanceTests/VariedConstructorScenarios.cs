62ba496c96c5f46c8de4a9c3de236634e6f79a67
Alex Novak <alexwnovak@gmail.com>
2017-07-29 12:40:07 -0400

    +A few scenarios for repeating parameters

--- src/NanoBuilder/NanoBuilder.AcceptanceTests/VariedConstructorScenarios.cs
+++ src/NanoBuilder/NanoBuilder.AcceptanceTests/VariedConstructorScenarios.cs
@@ -1,6 +1,33 @@
-﻿namespace NanoBuilder.AcceptanceTests
+﻿using System;
+using Xunit;
+using FluentAssertions;
+
+namespace NanoBuilder.AcceptanceTests
 {
    public class VariedConstructorScenarios
    {
+      [Fact]
+      public void CanBuildObjectWithManyIntegers()
+      {
+         var version = ObjectBuilder.For<Version>()
+            .With( 1 )
+            .With( 2 )
+            .With( 3 )
+            .Build();
+
+         version.ToString().Should().Be( "1.2.3" );
+      }
+
+      [Fact]
+      public void CanBuildObjectWithManyIntegersAndSkipTheMiddle()
+      {
+         var version = ObjectBuilder.For<Version>()
+            .With( 1 )
+            .Skip<int>()
+            .With( 3 )
+            .Build();
+
+         version.ToString().Should().Be( "1.0.3" );
+      }
    }
 }
